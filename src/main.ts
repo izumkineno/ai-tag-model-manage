93ed3f0c2d33698be9c05aa5594856a6d18dc0c4
izumki <1465707948@qq.com>
2023-01-21 10:39:42 +0800

    油猴注入测试

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import App from './App.vue'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 
+// // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// // @ts-ignore
+// const target = document.querySelector('body > gradio-app').shadowRoot.getElementById('toprow')
+// createApp(App)
+//   .use(createPinia())
+//   .mount(target as HTMLElement)
+
 createApp(App)
   .use(createPinia())
   .mount('#toprow')
