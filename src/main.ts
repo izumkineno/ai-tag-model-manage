90107b4dfaa6b39dea81ee412b4ed2c9fc09becd
izumki <1465707948@qq.com>
2023-01-21 21:06:56 +0800

    油猴注入测试,数据导入正向提示词测试

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import App from './App.vue'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const target = document.querySelector('body > gradio-app').shadowRoot.querySelector('#app') as HTMLElement
-createApp(App)
-  .use(createPinia())
-  .mount(target)
-
+// // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// // @ts-ignore
+// const target = document.querySelector('body > gradio-app').shadowRoot.querySelector('#app') as HTMLElement
 // createApp(App)
 //   .use(createPinia())
-//   .mount('#app')
+//   .mount(target)
+
+createApp(App)
+  .use(createPinia())
+  .mount('#app')
